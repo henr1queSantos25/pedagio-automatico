60acffe15b26fbd5c97e8c41e05fb4e50092f918
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-09-16 14:42:05 -0300

    Adiciona a matriz de led

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #define TRIGGER 16 // Pino trigger do sensor ultrassônico
 #define ECHO 17 // Pino echo do sensor ultrassônico
 
+// == BIBLIOTECAS PARA O DISPLAY LED ==
+#include "matriz.h"
 
 
 
@@ -20,8 +22,6 @@ void setup();
 int main() {
     setup();
 
-
-
     uint64_t distance;
     while (1) {
         distance = getCm(TRIGGER, ECHO);
@@ -30,10 +30,12 @@ int main() {
         } else {
             printf("Distância: %llu cm\n", distance);
             if (distance < 15) {
-                set_angle(CENTO_OITENTA_GRAUS); // Fecha a cancela
+                set_angle(CENTO_OITENTA_GRAUS); // Abre a cancela
+                matriz_atualizar_cor(0, 255, 0, 0.2);
             } else {
-                set_angle(NOVENTA_GRAUS); // Abre a cancela
-                sleep_ms(1500);
+                set_angle(NOVENTA_GRAUS); // Fecha a cancela
+                matriz_atualizar_cor(255, 0, 0, 0.2);
+
             }
         }
         sleep_ms(500);
@@ -49,6 +51,7 @@ int main() {
  */
 void setup() {
     stdio_init_all();
+    init_matriz();
     setupUltrasonicPins(TRIGGER, ECHO);
     setup_SG90();
 }
